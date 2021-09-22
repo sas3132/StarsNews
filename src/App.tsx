e0ca03875e2372beaa52729bcb81fa64dda2eed4
sas3132 <78425379+sas3132@users.noreply.github.com>
2021-09-22 20:01:24 +0300

    Начало. Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper clear">
+            <header className="header">
+                <div className="headerBurger">
+                    <img src="img/burgerImg.svg" alt=""/>
+                </div>
+                <img src="img/logo.svg" alt="" className="headerLogo"/>
+                <nav className="navBlock">
+                    <input placeholder={"Поиск..."} type="text" className="searchField"/>
+                    <button className="addNews"><div className="addButtonText">Добавить</div></button>
+                    <div className="headerSpacer"></div>
+                    <div className="userLogin">
+                        <img src="img/userImg.svg" alt="" className="userImg"/>
+                    </div>
+                </nav>
+            </header>
+            <main className="main">
+                <aside className="sidebar">
+                    <nav className="sidebarMenu">
+                        <ul className="sidebarList">
+                            <li>Популярное</li>
+                            <li>Новое</li>
+                        </ul>
+                        <ul className="newsTopics">
+                            <li>Starwars</li>
+                            <li>Новости города</li>
+                            <li>Водоканал</li>
+                        </ul>
+                    </nav>
+                    <footer className="footer">
+                        <div className="contact">
+                            <a href=""><img src="img/github.svg" alt=""/></a>
+                            <a href=""><img src="img/telegram.svg" alt=""/></a>
+                        </div>
+                        <div className="info">
+                            <p>by Sas31 © 2021. StarsNews.</p>
+                        </div>
+                    </footer>
+                </aside>
+                <section className="content">
+                    <div className="contentContainer">
+
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
